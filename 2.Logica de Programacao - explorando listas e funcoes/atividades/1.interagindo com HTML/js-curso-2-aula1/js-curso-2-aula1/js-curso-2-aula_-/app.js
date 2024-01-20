f57d37601ffa0317b20ac3ccdd69715c4ddf1450
kzzonato <jpcazzonato2013@gmail.com>
2024-01-20 01:02:58 -0300

    curso 2

---
+++ 2.Logica de Programacao - explorando listas e funcoes/atividades/1.interagindo com HTML/js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,12 @@
+let titulo = document.querySelector('h1');                                            // manipulando textos html
+titulo.innerHTML = 'Jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute(){
+    console.log('Clicou no botão');
+
+}
+
+
